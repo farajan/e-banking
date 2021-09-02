54e22395f0602ca932cf8f6ef881ce37aa6d87a2
farajan <jan.fara10@gmail.com>
2021-09-02 15:06:30 +0200

    PasswordEncoder added

--- user/src/main/java/com/example/ebanking/user/service/UserService.java
+++ user/src/main/java/com/example/ebanking/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.ebanking.user.service;
 
+import com.example.ebanking.user.config.PasswordEncoderConfig;
 import com.example.ebanking.user.db.entity.User;
 import com.example.ebanking.user.dto.UserRequest;
 import com.example.ebanking.user.db.repository.UserRepository;
@@ -8,6 +9,7 @@ import com.example.ebanking.user.service.webClient.InsuranceWebClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
@@ -21,6 +23,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final InsuranceWebClient insuranceWebClient;
     private final BankWebClient bankWebClient;
+    private final PasswordEncoder passwordEncoder;
 
     public Page<User> findAll(Pageable pageable) {
         return userRepository.findAll(pageable);
@@ -39,7 +42,7 @@ public class UserService {
         user.setFirstName(userRequest.getFirstName());
         user.setLastName(userRequest.getLastName());
         user.setUsername(userRequest.getUsername());
-        user.setPassword(userRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
         user.setEmail(userRequest.getEmail());
         user.setBirthday(userRequest.getBirthday());
 
