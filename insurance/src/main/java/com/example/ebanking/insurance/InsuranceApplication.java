d8f0e75877bfb1e4c5b2e5753d6ee98d2f234295
farajan <jan.fara10@gmail.com>
2021-09-02 14:57:05 +0200

    WebClientConfig class added

--- insurance/src/main/java/com/example/ebanking/insurance/InsuranceApplication.java
+++ insurance/src/main/java/com/example/ebanking/insurance/InsuranceApplication.java
@@ -3,10 +3,6 @@ package com.example.ebanking.insurance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -14,13 +10,6 @@ public class InsuranceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InsuranceApplication.class, args);
-	}
-
-	@Bean
-	public WebClient webClient() {
-		return WebClient.builder()
-				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-				.build();
 	}
 
 }
